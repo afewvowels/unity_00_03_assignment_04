31b1c56f81283ed455ef5d310b46850bae99fcec
Keith Smith <keithbsmith87@gmail.com>
2019-11-04 08:22:09 -0500

    update

--- YSU_CSIS_5838/Assets/Scripts/delete_enemy.cs
+++ YSU_CSIS_5838/Assets/Scripts/delete_enemy.cs
@@ -1,25 +1,62 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class delete_enemy : MonoBehaviour
 {
 	private GameObject scene;
 	private Text score;
+	private float decrement;
+	private bool isHit;
+	private bool isZero;
 
 	void Start()
 	{
 		scene = GameObject.FindGameObjectWithTag("scene");
 		score = GameObject.FindGameObjectWithTag("score").GetComponent<Text>();
+		decrement = 0.05f;
+		isHit = false;
+		isZero = false;
+	}
+
+	private void FixedUpdate()
+	{
+		if (isHit == true)
+		{
+			this.GetComponent<NavMeshAgent>().enabled = false;
+			ScaleDown();
+		}
+
+		if (isZero == true)
+		{
+			DeleteEnemy();
+		}
 	}
 
 	private void OnCollisionEnter(Collision collision)
 	{
 		if (collision.gameObject.CompareTag("projectile"))
 		{
-			Destroy(this.gameObject);
-			scene.GetComponent<enemy_count>().DecrementCount();
-			score.text = Convert.ToString(Convert.ToInt32(score.text) + 5);
+			isHit = true;
 		}
+	}
+
+	private void ScaleDown()
+	{
+		this.transform.Rotate(0.0f, 10.0f, 0.0f);
+		this.transform.localScale -= new Vector3(decrement, decrement, decrement);
+
+		if (this.transform.localScale.x <= 0.0f)
+		{
+			isZero = true;
+		}
+	}
+
+	private void DeleteEnemy()
+	{
+		Destroy(this.gameObject);
+		scene.GetComponent<enemy_count>().DecrementCount();
+		score.text = Convert.ToString(Convert.ToInt32(score.text) + 5);
 	}
 }
